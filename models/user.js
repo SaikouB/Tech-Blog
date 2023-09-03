ffc8928f1abf1403274a6286e6f31293b45e3f2d
Smooth Operator <bsaikou1990@gmail.com>
2023-09-02 20:59:03 -0400

    Added data to models files

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3],
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+                len: [3],
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
+        },
+	},
+    {
+        hooks: {
+            async beforeCreate(newUSer) {
+                newUSer.password = await bcrypt.hash(newUSer.password, 10);
+                return newUSer;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
